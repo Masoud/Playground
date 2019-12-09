19389d54e9fd20a8e985010c72a2482bf4dd7996
masoudnikoomanesh <masoudnikoomanesh@gmail.com>
2019-12-09 15:13:40 +0330

    update: function after keyup on search

--- react-cdnlib/src/Search.js
+++ react-cdnlib/src/Search.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
 
 class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { search: '' };
+
+        this.SearchIT = this.SearchIT.bind(this);
+    }
+
+    state= {
+        search: '',
+        typing: false,
+        typingTimeout: 0
+    }
+    SearchIT(event) {
+        const self = this;
+
+        if (self.state.typingTimeout) {
+            clearTimeout(self.state.typingTimeout);
+        }
+
+        self.setState({
+            search: event.target.value,
+            typing: false,
+            typingTimeout: setTimeout(function () {
+                self.afterSetStateFinished();
+            }, 1500)
+        });
+    }
+
+
+
+    afterSetStateFinished() {
+        alert('hi');
+    }
+
     render() {
         return (
             <div className="row">
-                <form className="search_body" action="/" method="get">
-                    <input className="search_tx" type="text" name="s" placeholder="Search Any Libraries ..." />
+                <form className="search_body">
+                    <input className="search_tx" type="text" placeholder="Search Any Libraries ..."
+                        value={this.state.value}
+                        onKeyDown={this.SearchIT}
+                    />
                     <button className="icon-search" value=" " type="submit"></button>
                 </form>
             </div>
