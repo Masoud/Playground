081455941af5482ca3452bb4770e871d372131c6
masoudnikoomanesh <masoudnikoomanesh@gmail.com>
2019-12-09 12:23:35 +0330

    update: all favorits section

--- react-cdnlib/src/Favorites.js
+++ react-cdnlib/src/Favorites.js
@@ -6,40 +6,155 @@ class Favorites extends Component {
             <div className="container">
                 <div className="row">
                     <div className="favorits">
-                        <div className="title">
-                            <h2>Favorite JavaScript Libraries</h2>
+                        <div className="section">
+                            <div className="title">
+                                <h2>Favorite JavaScript Libraries</h2>
+                                <a href="/">See All</a>
+                            </div>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Link</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>angular-formly-material</td>
+                                        <td>http://cdnlib.net/ar/angular-formly-material/0.9.0/formly-material.min.js</td>
+                                    </tr>
+                                    <tr>
+                                        <td>styled-components</td>
+                                        <td>http://cdnlib.net/ar/styled-components/5.0.0-beta.8/styled-components.browser.cjs.min.js</td>
+                                    </tr>
+                                    <tr>
+                                        <td>sticky-kit</td>
+                                        <td>http://cdnlib.net/ar/sticky-kit/1.1.3/sticky-kit.min.js</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Vue.Draggable</td>
+                                        <td>http://cdnlib.net/ar/Vue.Draggable/2.9.0/vuedraggable.min.js</td>
+                                    </tr>
+                                    <tr>
+                                        <td>darkroomjs</td>
+                                        <td>http://cdnlib.net/ar/darkroomjs/2.0.1/darkroom.css</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Link</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>angular-formly-material</td>
-                                    <td>http://cdnlib.net/ar/angular-formly-material/0.9.0/formly-material.min.js</td>
-                                </tr>
-                                <tr>
-                                    <td>styled-components</td>
-                                    <td>http://cdnlib.net/ar/styled-components/5.0.0-beta.8/styled-components.browser.cjs.min.js</td>
-                                </tr>
-                                <tr>
-                                    <td>sticky-kit</td>
-                                    <td>http://cdnlib.net/ar/sticky-kit/1.1.3/sticky-kit.min.js</td>
-                                </tr>
-                                <tr>
-                                    <td>Vue.Draggable</td>
-                                    <td>http://cdnlib.net/ar/Vue.Draggable/2.9.0/vuedraggable.min.js</td>
-                                </tr>
-                                <tr>
-                                    <td>darkroomjs</td>
-                                    <td>http://cdnlib.net/ar/darkroomjs/2.0.1/darkroom.css</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <div className="section">
+                            <div className="title">
+                                <h2>Favorite CSS Libraries</h2>
+                                <a href="/">See All</a>
+                            </div>
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Link</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>angular-formly-material</td>
+                                        <td>http://cdnlib.net/ar/angular-formly-material/0.9.0/formly-material.min.js</td>
+                                    </tr>
+                                    <tr>
+                                        <td>styled-components</td>
+                                        <td>http://cdnlib.net/ar/styled-components/5.0.0-beta.8/styled-components.browser.cjs.min.js</td>
+                                    </tr>
+                                    <tr>
+                                        <td>sticky-kit</td>
+                                        <td>http://cdnlib.net/ar/sticky-kit/1.1.3/sticky-kit.min.js</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Vue.Draggable</td>
+                                        <td>http://cdnlib.net/ar/Vue.Draggable/2.9.0/vuedraggable.min.js</td>
+                                    </tr>
+                                    <tr>
+                                        <td>darkroomjs</td>
+                                        <td>http://cdnlib.net/ar/darkroomjs/2.0.1/darkroom.css</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="section fonts">
+                            <div className="title">
+                                <h2>Favorite Persian Fonts Libraries</h2>
+                                <a href="/">See All</a>
+                            </div>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>angular-formly-material</td>
+                                    </tr>
+                                    <tr>
+                                        <td>styled-components</td>
+                                    </tr>
+                                    <tr>
+                                        <td>sticky-kit</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Vue.Draggable</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Vue.Draggable</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="section fonts">
+                            <div className="title">
+                                <h2>Favorite Arabic Fonts Libraries</h2>
+                                <a href="/">See All</a>
+                            </div>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>angular-formly-material</td>
+                                    </tr>
+                                    <tr>
+                                        <td>styled-components</td>
+                                    </tr>
+                                    <tr>
+                                        <td>sticky-kit</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Vue.Draggable</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Vue.Draggable</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="section fonts">
+                            <div className="title">
+                                <h2>Favorite English Fonts Libraries</h2>
+                                <a href="/">See All</a>
+                            </div>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>angular-formly-material</td>
+                                    </tr>
+                                    <tr>
+                                        <td>styled-components</td>
+                                    </tr>
+                                    <tr>
+                                        <td>sticky-kit</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Vue.Draggable</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Vue.Draggable</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
+                    
                 </div>
             </div>
         )
